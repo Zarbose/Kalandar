486c7f270b2adf0146e689db4bd19c524465d2bb
Zarbose <simonpieto.pieto@gmail.com>
2023-01-19 16:55:25 +0100

    add rdv functional

--- kalandar-api/src/main/java/df/calendar/RdvsResource.java
+++ kalandar-api/src/main/java/df/calendar/RdvsResource.java
@@ -3,14 +3,14 @@ package df.calendar;
 
 import df.calendar.model.Rdv;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Request;
 import javax.ws.rs.core.UriInfo;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Path("/rdv")
@@ -26,7 +26,7 @@ public class RdvsResource {
     @Produces(MediaType.TEXT_XML)
     public List<Rdv> getRdvListe() {
         List<Rdv> tousRdv = new ArrayList<Rdv>();
-        tousRdv.addAll(DaoRdv.instance.getRdv());
+        tousRdv.addAll(DaoRdv.instance.getRdvs().values());
         return tousRdv;
     }
 
@@ -35,7 +35,7 @@ public class RdvsResource {
     public String getRdvList2() {
         StringBuffer result = new StringBuffer("<html><body><h1>Rdv</h1><ul>");
         String loc = uriInfo.getAbsolutePath().toString();
-        for (Rdv r : DaoRdv.instance.getRdv()) {
+        for (Rdv r : DaoRdv.instance.getRdvs().values()) {
             result.append("<li><a href=\""+loc+ "/"+ r.getId()+"\">"
                     +loc+ "/"+ r.getId()+"</a></li>");
         }
@@ -46,7 +46,23 @@ public class RdvsResource {
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
     public List<Rdv> getRdv() {
         List<Rdv> allRdv = new ArrayList<Rdv>();
-        allRdv.addAll(DaoRdv.instance.getRdv());
+        allRdv.addAll(DaoRdv.instance.getRdvs().values());
         return allRdv;
     }
+
+    @POST
+    @Produces(MediaType.TEXT_XML)
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public String newRdv(@FormParam("id") String id, @FormParam("title") String title) throws IOException {
+        Rdv rdv = new Rdv(id,title,"blabla",new Date(), new Date());
+        DaoRdv.instance.getRdvs().put(rdv.getId(),rdv);
+        return "<?xml version=\"1.0\"?>" + "<links> <href>" +
+                uriInfo.getAbsolutePath() + "/"+ id + " </href> </links>";
+    }
+
+    @Path("{id}")
+    public RdvRessource getRdv(@PathParam("id") String id) {
+        return new RdvRessource(uriInfo, request, id);
+    }
+
 }
